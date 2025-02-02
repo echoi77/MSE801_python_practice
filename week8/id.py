50e8ba25be8dccea3118ea89859c8a1db39218c5
echoi77 <choieunseok77@gmail.com>
2025-02-03 11:57:26 +1300

    update

--- week8/id.py
+++ week8/id.py
@@ -8,3 +8,27 @@ k = Calculator()
 a = k.add(1,2)
 
 print(a)
+
+
+class Calc:
+    def __init__(self):
+        self.result = 0
+
+    def add(self, num):
+        self.result = self.result + num
+        return self.result
+
+    def sub(self, num):
+
+        self.result = self.result - num
+        return self.result
+
+k = Calc()
+min = Calc()
+
+print(k.add(3))
+print(k.add(5))
+
+
+print(min.sub(5))
+print(min.sub(3))
