5897627f0842ffa4e7f41e8857658d4cfcbea36c
echoi77 <choieunseok77@gmail.com>
2025-02-24 14:36:34 +1300

    adding new button

--- Week13/flask_web_app/static/script.js
+++ Week13/flask_web_app/static/script.js
@@ -1,3 +1,8 @@
 document.getElementById('alertBtn').addEventListener('click', function() {
     alert('Hello from JavaScript!');
 });
+
+
+document.getElementById('alertBtn1').addEventListener('click', function() {
+    alert('Bye from JavaScript!');
+});
